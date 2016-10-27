3d87563e762c76443a55f6f7a674f46975057f06
alexFickle <alex.fickle@gmail.com>
2016-10-27 17:52:16 -0500

    first commit

---
+++ private.py
@@ -0,0 +1,3 @@
+bot_email = '' #put the bot's email here
+bot_password = '' #put the bot's password here
+master = '000000000000000000' # put the owner's discord user id here
---
+++ rowboot.py
@@ -0,0 +1,119 @@
+import discord
+client = discord.Client()
+
+import time
+start = time.time()
+import datetime
+start_date = datetime.datetime.utcnow()
+
+import private
+bot_email = private.bot_email
+bot_password = private.bot_password
+master = private.master
+
+
+frac_list = [ 
+'Solid Ocean, Underground Facility, Urban Battleground',
+'Aetherblade, Thaumanova Reactor, Molten Furnace',
+'Cliffside, Molten Boss, Captain Mai Trin Boss',
+'Swampland, Solid Ocean, Uncategorized',
+'Aquatic Ruins, Snowblind, Thaumanova Reactor',
+'Aetherblade, Uncategorized, Volcanic',
+'Cliffside, Urban Battleground, Chaos Isles',
+'Underground Facility, Volcanic, Captain Mai Trin Boss',
+'Snowblind, Solid Ocean, Swampland',
+'Chaos Isles, Uncategorized, Urban Battleground',
+'Cliffside, Molten Furnace, Captain Mai Trin Boss',
+'Underground Facility, Thaumanova Reactor, Molten Boss',
+'Volcanic, Swampland, Aetherblade',
+'Snowblind, Chaos Isles, Aquatic Ruins'
+];
+
+
+
+pact_supply_network_locations = [
+'[&BIkHAAA=] [&BC0AAAA=] [&BDoBAAA=] [&BO4CAAA=] [&BIUCAAA=] [&BCECAAA=]',
+'[&BIcHAAA=] [&BKYBAAA=] [&BEwDAAA=] [&BNIEAAA=] [&BIMCAAA=] [&BA8CAAA=]',
+'[&BH8HAAA=] [&BBkAAAA=] [&BEgAAAA=] [&BKgCAAA=] [&BGQCAAA=] [&BIMBAAA=]',
+'[&BH4HAAA=] [&BKYAAAA=] [&BMIBAAA=] [&BP0CAAA=] [&BDgDAAA=] [&BPEBAAA=]',
+'[&BKsHAAA=] [&BIMAAAA=] [&BE8AAAA=] [&BP0DAAA=] [&BF0GAAA=] [&BOcBAAA=]',
+'[&BJQHAAA=] [&BNUGAAA=] [&BMMCAAA=] [&BJsCAAA=] [&BHsBAAA=] [&BNMAAAA=]',
+'[&BH8HAAA=] [&BJIBAAA=] [&BLkCAAA=] [&BBEDAAA=] [&BEICAAA=] [&BBABAAA=]'
+];
+
+def reply(author, message, msg, talk):
+	yield from client.send_message(message.channel,msg,tts=talk)
+
+def join(author, message):
+	if author.id == master:
+		join_link = message.content.strip('!join ')
+		print('attempting join: %s' % join_link )
+		yield from client.accept_invite(join_link)
+		print('sucess!\n')
+		msg = "joined"
+		yield from reply(author, message, msg, False)
+	else:
+		msg = "{0} can't tell me what to do".format(author)
+		yield from reply(author, message, msg, False)
+
+def test(author, message):
+	print("test requst from {0} in {1}\n".format(author, message.channel))
+	msg = "Hello {0}.  Use !help for a list of commands".format(author)
+	yield from reply(author, message, msg, False)
+	
+def leave(author, message):
+	if author.id == master:
+		print('leaving: %s' % message.server)
+		msg = 'goodbye'
+		yield from reply(author, message, msg, False)
+		yield from client.leave_server(message.server)
+	else:
+		msg = 'no, you'
+		yield from reply(author, message, msg, False)
+
+def frac(author, message,date):
+	print('Fractal request received')
+	print("Time: {0}:{1}:{2}".format(date.timetuple()[3],date.timetuple()[4],date.timetuple()[5]))
+	print("Week: {0}, Day of Week: {1}".format(date.isocalendar()[1],date.isocalendar()[2]))
+	daily_num = date.isocalendar()[2] - 1 + ((date.isocalendar()[1] % 2) * 7)
+	print("{0}: {1}".format(daily_num, frac_list[daily_num]))
+	msg = 'The daily fractals are %s.' % frac_list[daily_num]
+	print('%s\n' % msg)
+	yield from reply(author, message, msg, False)
+
+def help(author, message):
+	print("{0} needs some help\n".format(author))
+	msg = "Command list: !frac, !pact, !help, !test"
+	yield from reply(author, message, msg, False)
+	if author.id == master:
+		msg = "Master only commands: !join, !fuck_off"
+		yield from reply(author, message, msg, False)
+		
+def pact(author, message,date):
+	print("Pact Supply Network Agent request received")
+	print("Time: {0}:{1}:{2}".format(date.timetuple()[3],date.timetuple()[4],date.timetuple()[5]))
+	daily_num = date.isocalendar()[2] - 1
+	if date.timetuple()[3] > 8:
+		daily_num = (daily_num + 1) % 7
+	print("{0}: {1}\n".format(daily_num,pact_supply_network_locations[daily_num]))
+	yield from reply(author, message, pact_supply_network_locations[daily_num], False)
+
+@client.async_event
+def on_message(message):
+	author = message.author
+	date = start_date + datetime.timedelta( seconds = (time.time() - start))
+	if message.content.startswith('!test'):
+		yield from test(author, message)
+	elif message.content.startswith('!join'):
+		yield from join(author, message)
+	elif message.content.startswith('!fuck_off'):
+		yield from leave(author, message)
+	elif message.content.startswith('!frac'):
+		yield from frac(author, message,date)
+	elif message.content.startswith('!help'):
+		yield from help(author, message)
+	elif message.content.startswith('!pact'):
+		yield from pact(author, message,date)
+		
+print("beep boop\n")
+client.run(bot_email,bot_password)
